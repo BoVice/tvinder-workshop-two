cab340919245dce3cef3bf831db19ccbf5e1c065
busster <jasonmbuss@gmail.com>
2017-08-04 09:29:55 -0400

    Add event emitters and listeners for actions component, pass likes as prop

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 ((() => {
   const html = `
     <div>
-      <app-header></app-header>
+      <app-header :likes="likes"></app-header>
       <movies :image_url="image_url"></movies>
+      <actions @handleLikes="handleLikes" @handleSkip="handleSkip"></actions>
     </div>
   `
 
@@ -10,8 +11,27 @@
     template: html,
     data(){
       return {
-        image_url: "https://goo.gl/Puw6Ar"
+        image_url: "https://goo.gl/Puw6Ar",
+        likes: 0,
+      }
+    },
+
+    methods: {
+      handleLikes(vote) {
+        const self = this
+        self.likes += vote
+        self.incrementImage()
+      },
+
+      handleSkip() {
+        const self = this
+        self.incrementImage()
+      },
+
+      incrementImage() {
+        // TODO
       }
     }
+
   })
 }))()
